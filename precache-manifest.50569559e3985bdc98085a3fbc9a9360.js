82b75ab07fe4d1bd5a1f50899e5c1afa629a7dce
Stas <nevmyvakastas@gmail.com>
2020-07-30 11:07:48 +0600

    Updates

---
+++ precache-manifest.50569559e3985bdc98085a3fbc9a9360.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e8ca57798d82f6f826b923e48c87e5",
+    "url": "/crypto-converter/index.html"
+  },
+  {
+    "revision": "e795e8edf4caa02b6fca",
+    "url": "/crypto-converter/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "ec1dd7a79cd7e48155d3",
+    "url": "/crypto-converter/static/js/2.890cefee.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/crypto-converter/static/js/2.890cefee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e795e8edf4caa02b6fca",
+    "url": "/crypto-converter/static/js/main.be45dfc0.chunk.js"
+  },
+  {
+    "revision": "7d7523149c87601c39e4",
+    "url": "/crypto-converter/static/js/runtime-main.c94f6d20.js"
+  }
+]);
